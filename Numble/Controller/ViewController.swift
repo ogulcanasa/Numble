b02c6e9dd28eb03573483fdc94492d2d7a976d17
Ogulcan Asa <ogulcanasa@hotmail.com>
2022-02-21 15:04:41 +0300

    Numbers are added.

--- Numble/Controller/ViewController.swift
+++ Numble/Controller/ViewController.swift
@@ -13,6 +13,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var notesLabel: UILabel!
     @IBOutlet weak var buttonLabel: UIButton!
     
+    @IBOutlet weak var zeroTF: UIButton!
+    @IBOutlet weak var oneTF: UIButton!
+    @IBOutlet weak var twoTF: UIButton!
+    @IBOutlet weak var threeTF: UIButton!
+    @IBOutlet weak var fourTF: UIButton!
+    @IBOutlet weak var fiveTF: UIButton!
+    @IBOutlet weak var sixTF: UIButton!
+    @IBOutlet weak var sevenTF: UIButton!
+    @IBOutlet weak var eightTF: UIButton!
+    @IBOutlet weak var nineTF: UIButton!
+    
+    
     var number = ""
     var numberInt : Int!
     var userNumberArray = [Int]()
@@ -70,7 +82,7 @@ class ViewController: UIViewController {
                 }
             }
             minus = common - plus
-            var newNote = "\(numberTextField.text!) ( \(plus) / \(-minus))"
+            let newNote = "\(numberTextField.text!) ( \(plus) / \(-minus))"
             notes = notes + "\n \(newNote)"
             notesLabel.text = notes
             numberTextField.text = ""
@@ -84,6 +96,12 @@ class ViewController: UIViewController {
             numberTextField.backgroundColor = UIColor.green
         }
     }
+    
+    @IBAction func numberButtonClicked(_ sender: UIButton) {
+        sender.tintColor = UIColor.white
+    }
+    
+    
     
     @IBAction func restartButtonClicked(_ sender: Any) {
         buttonLabel.titleLabel?.text = "Make a guess!"
@@ -104,6 +122,16 @@ class ViewController: UIViewController {
         numberTextField.backgroundColor = UIColor.white
         numberTextField.isHidden = false
         buttonLabel.isHidden = false
+        zeroTF.tintColor = UIColor.black
+        oneTF.tintColor = UIColor.black
+        twoTF.tintColor = UIColor.black
+        threeTF.tintColor = UIColor.black
+        fourTF.tintColor = UIColor.black
+        fiveTF.tintColor = UIColor.black
+        sixTF.tintColor = UIColor.black
+        sevenTF.tintColor = UIColor.black
+        eightTF.tintColor = UIColor.black
+        nineTF.tintColor = UIColor.black
     }
 }
 
