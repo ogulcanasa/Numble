f158002c763c9f09b9e7f0a92c3d95cc4a658076
Ogulcan Asa <ogulcanasa@hotmail.com>
2022-08-30 22:46:17 +0300

    Code is edited.

--- Numble/Controller/ViewController.swift
+++ Numble/Controller/ViewController.swift
@@ -16,9 +16,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var changeButtonLabel: UIButton!
     @IBOutlet weak var selectedZeroTF,selectedOneTF,selectedTwoTF,selectedThreeTF,selectedFourTF,selectedFiveTF,selectedSixTF,selectedSevenTF,selectedEightTF,selectedNineTF: UIButton!
     @IBOutlet weak var zeroTF,oneTF,twoTF,threeTF,fourTF,fiveTF,sixTF,sevenTF,eightTF,nineTF: UIButton!
-
-    var number = ""
-    var numberInt : Int!
+    
+    var selectedbTFButton: [UIButton] {
+        return [selectedZeroTF,selectedOneTF,selectedTwoTF,selectedThreeTF,selectedFourTF,selectedFiveTF,selectedSixTF,selectedSevenTF,selectedEightTF,selectedNineTF]
+    }
+    var textFieldButton: [UIButton] {
+        return [zeroTF,oneTF,twoTF,threeTF,fourTF,fiveTF,sixTF,sevenTF,eightTF,nineTF]
+    }
+    var number = 0
     var userNumberArray = [Int]()
     var randomArray = [Int]()
     var notes = ""
@@ -29,101 +34,55 @@ class ViewController: UIViewController {
     var minus = 0
     private var numberOfClicked = 0
     var selectedNumber = ""
-    private var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         numberTextField.isEnabled = false
-        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        buttonLabel.isEnabled = false
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
            view.addGestureRecognizer(tap)
+        while set.count < 4 {
+            set.insert(Int.random(in: 0...9))
+        }
+        randomArray = Array(set)
     }
     
     @IBAction func selectedNumberButtonClicked(_ sender: UIButton) {
-        timer.invalidate()
-        selectedNumber = selectedNumber + (sender.titleLabel!.text!)
+        selectedNumber += sender.titleLabel!.text!
         numberTextField.text = selectedNumber
         if (numberTextField.text!.count) < 4 {
         sender.isEnabled = false
         } else if selectedNumber.count == 4 {
             disableOfSelectedNumbers()
+            buttonLabel.isEnabled = true
         }
     }
     
     @IBAction func guessButtonClicked(_ sender: Any) {
-        (sender as AnyObject).setTitle("Continue to guess...", for: .normal)
-        number = numberTextField.text ?? "0000"
-        numberInt = Int(number) ?? 0000
-        
-        if numberTextField.text == "" {
-            userNumberArray = [0,0,0,0]
-            common = 0
-            minus = 0
-            plus = 0
-            numberTextField.text = "0000"
-        } else if numberTextField.text!.count < 4{
-            userNumberArray = [0,0,0,0]
-            numberTextField.text = "0000"
+        if selectedNumber.count == 4 {
+            buttonLabel.isEnabled = false
         } else {
-            userNumberArray = [Int(numberInt/1000), Int((numberInt%1000)/100) , Int((numberInt%100)/10) , Int(numberInt%10)]
+            buttonLabel.isEnabled = true
         }
+        numberOfClicked += 1
+        number = Int(numberTextField.text ?? "0000") ?? 0000
 
-        if (buttonLabel.titleLabel?.text == "Make a guess!") {
-            enableOfSelectedNumbers()
-            numberOfClicked += 1
-            while set.count < userNumberArray.count {
-                set.insert(Int.random(in: 0...9))
-                }
-            randomArray = Array(set)
-            for i in 0...3 {
-                if randomArray[i] == userNumberArray[i] {
-                    plus += 1 }
-                for j in 0...3 {
-                    if randomArray[i] == userNumberArray[j] {
-                        common += 1
-                    }
-                }
-            }
-            if set.count == 0 {
-                notes = "\(numberTextField.text!) ( 0 / 0 )"
-                notesLabel.text = notes
-                numberTextField.text = ""
-            } else {
-            minus = common - plus
-            if plus == 0 {
-            newNote = "\(numberTextField.text!) ( \(plus) / \(-minus))"
-            } else {
-            newNote = "\(numberTextField.text!) ( +\(plus) / \(-minus))"
-            }
-            notes = notes + "\n \(newNote)"
-            notesLabel.text = notes
-            numberTextField.text = ""
-            print(randomArray)
-            }
-        } else {
-            enableOfSelectedNumbers()
-            numberOfClicked += 1
-            common = 0
-            minus = 0
-            plus = 0
-            for i in 0...3 {
-                if randomArray[i] == userNumberArray[i] {
-                    plus += 1 }
-                for j in 0...3 {
-                    if randomArray[i] == userNumberArray[j] {
-                        common += 1
-                    }
-                }
-            }
-            minus = common - plus
-            if plus == 0 {
-            newNote = "\(numberTextField.text!) ( \(plus) / \(-minus))"
-            } else {
-            newNote = "\(numberTextField.text!) ( +\(plus) / \(-minus))"
-            }
-            notes = notes + "\n \(newNote)"
-            notesLabel.text = notes
-            numberTextField.text = ""
-        }
+        userNumberArray = [Int(number/1000),Int((number%1000)/100),Int((number%100)/10),Int(number%10)]
+
+        enableOfSelectedNumbers()
+        
+        for i in 0...3 {
+            if randomArray[i] == userNumberArray[i] {
+                plus += 1 }
+            for j in 0...3 {
+                if randomArray[i] == userNumberArray[j] {
+                    common += 1
+        }}}
+
+        minus = common - plus
+        newNote = "\(numberTextField.text!) ( +\(plus) / \(-minus))"
+        notes = notes + "\n \(newNote)"
+        notesLabel.text = notes
         
         if plus == 4 {
             notesLabel.text = "CONGRATULATIONS!! \n TRIAL: \(numberOfClicked)"
@@ -142,12 +101,17 @@ class ViewController: UIViewController {
             buttonLabel.isHidden = true
             view.backgroundColor = UIColor.red
             numberTextField.backgroundColor = UIColor.red
-            makeTFRed()
             makeSelectedTFRed()
+            makeTFRed()
+            changeButtonLabel.tintColor = UIColor.red
         }
+        numberTextField.text = ""
+        minus = 0
+        common = 0
+        plus = 0
         selectedNumber = ""
     }
-    
+
     @IBAction func numberButtonClicked(_ sender: UIButton) {
         sender.tintColor = UIColor.systemTeal
     }
@@ -161,13 +125,11 @@ class ViewController: UIViewController {
     @IBAction func restartButtonClicked(_ sender: Any) {
         let alert = UIAlertController(title: "Warning!", message: "Do you want to restart the game", preferredStyle: .alert)
         let defaultAction = UIAlertAction(title: "Yes", style: UIAlertAction.Style.default, handler: { [self] action in
-            buttonLabel.titleLabel?.text = "Make a guess!"
-            set = []
-            while set.count < userNumberArray.count {
+            set.removeAll()
+            while set.count < 4 {
                 set.insert(Int.random(in: 0...9))
-                }
+            }
             randomArray = Array(set)
-            print(set)
             notes = ""
             notesLabel.text = "Notes"
             common = 0
@@ -196,108 +158,43 @@ class ViewController: UIViewController {
     }
     
     func makeSelectedTFBlack() {
-        selectedZeroTF.tintColor = UIColor.black
-        selectedOneTF.tintColor = UIColor.black
-        selectedTwoTF.tintColor = UIColor.black
-        selectedThreeTF.tintColor = UIColor.black
-        selectedFourTF.tintColor = UIColor.black
-        selectedFiveTF.tintColor = UIColor.black
-        selectedSixTF.tintColor = UIColor.black
-        selectedSevenTF.tintColor = UIColor.black
-        selectedEightTF.tintColor = UIColor.black
-        selectedNineTF.tintColor = UIColor.black
+        for textField in selectedbTFButton {
+            textField.tintColor = UIColor.black
+        }
     }
     
     func makeSelectedTFRed() {
-        selectedZeroTF.tintColor = UIColor.red
-        selectedOneTF.tintColor = UIColor.red
-        selectedTwoTF.tintColor = UIColor.red
-        selectedThreeTF.tintColor = UIColor.red
-        selectedFourTF.tintColor = UIColor.red
-        selectedFiveTF.tintColor = UIColor.red
-        selectedSixTF.tintColor = UIColor.red
-        selectedSevenTF.tintColor = UIColor.red
-        selectedEightTF.tintColor = UIColor.red
-        selectedNineTF.tintColor = UIColor.red
-        changeButtonLabel.tintColor = UIColor.red
-    }
+        for textField in selectedbTFButton {
+            textField.tintColor = UIColor.red
+    }}
     
     func makeSelectedTFGreen() {
-        selectedZeroTF.tintColor = UIColor.green
-        selectedOneTF.tintColor = UIColor.green
-        selectedTwoTF.tintColor = UIColor.green
-        selectedThreeTF.tintColor = UIColor.green
-        selectedFourTF.tintColor = UIColor.green
-        selectedFiveTF.tintColor = UIColor.green
-        selectedSixTF.tintColor = UIColor.green
-        selectedSevenTF.tintColor = UIColor.green
-        selectedEightTF.tintColor = UIColor.green
-        selectedNineTF.tintColor = UIColor.green
-    }
+        for textField in selectedbTFButton {
+            textField.tintColor = UIColor.green
+    }}
     
     func enableOfSelectedNumbers() {
-        selectedZeroTF.isEnabled = true
-        selectedOneTF.isEnabled = true
-        selectedTwoTF.isEnabled = true
-        selectedThreeTF.isEnabled = true
-        selectedFourTF.isEnabled = true
-        selectedFiveTF.isEnabled = true
-        selectedSixTF.isEnabled = true
-        selectedSevenTF.isEnabled = true
-        selectedEightTF.isEnabled = true
-        selectedNineTF.isEnabled = true
-    }
+        for textField in selectedbTFButton {
+            textField.isEnabled = true
+    }}
     
     func disableOfSelectedNumbers() {
-        selectedZeroTF.isEnabled = false
-        selectedOneTF.isEnabled = false
-        selectedTwoTF.isEnabled = false
-        selectedThreeTF.isEnabled = false
-        selectedFourTF.isEnabled = false
-        selectedFiveTF.isEnabled = false
-        selectedSixTF.isEnabled = false
-        selectedSevenTF.isEnabled = false
-        selectedEightTF.isEnabled = false
-        selectedNineTF.isEnabled = false
-    }
+        for textField in selectedbTFButton {
+            textField.isEnabled = false
+    }}
     
     func makeTFRed() {
-        zeroTF.tintColor = UIColor.red
-        oneTF.tintColor = UIColor.red
-        twoTF.tintColor = UIColor.red
-        threeTF.tintColor = UIColor.red
-        fourTF.tintColor = UIColor.red
-        fiveTF.tintColor = UIColor.red
-        sixTF.tintColor = UIColor.red
-        sevenTF.tintColor = UIColor.red
-        eightTF.tintColor = UIColor.red
-        nineTF.tintColor = UIColor.red
-    }
+        for textField in textFieldButton {
+            textField.tintColor = UIColor.red
+    }}
     
     func makeTFGreen() {
-        zeroTF.tintColor = UIColor.green
-        oneTF.tintColor = UIColor.green
-        twoTF.tintColor = UIColor.green
-        threeTF.tintColor = UIColor.green
-        fourTF.tintColor = UIColor.green
-        fiveTF.tintColor = UIColor.green
-        sixTF.tintColor = UIColor.green
-        sevenTF.tintColor = UIColor.green
-        eightTF.tintColor = UIColor.green
-        nineTF.tintColor = UIColor.green
-    }
+        for textField in textFieldButton {
+            textField.tintColor = UIColor.green
+    }}
     
     func changeColorOfTintColorToBlack() {
-        zeroTF.tintColor = UIColor.black
-        oneTF.tintColor = UIColor.black
-        twoTF.tintColor = UIColor.black
-        threeTF.tintColor = UIColor.black
-        fourTF.tintColor = UIColor.black
-        fiveTF.tintColor = UIColor.black
-        sixTF.tintColor = UIColor.black
-        sevenTF.tintColor = UIColor.black
-        eightTF.tintColor = UIColor.black
-        nineTF.tintColor = UIColor.black
-        zeroTF.tintColor = UIColor.black
-    }
+        for textField in textFieldButton {
+            textField.tintColor = UIColor.black
+    }}
 }
